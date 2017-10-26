f2cc894014e06e9736f7c80a9f067a4daeac135c
Andrei Raiski <RaiskiA@IBA>
2017-10-26 16:50:49 +0300

    small fix

--- zeep1.py
+++ zeep1.py
@@ -4,4 +4,4 @@ client = Client('http://www.webservicex.net/ConvertSpeed.asmx?WSDL')
 result = client.service.ConvertSpeed(
     100, 'kilometersPerhour', 'centimetersPersecond')
 print(result)
-# assert result == 62.137
+assert result == 62.137
